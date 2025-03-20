8056cbd657e9134f463839770782ae83130e5819
Team Wolfmare Official <teamwolfmareofficial@gmail.com>
2025-03-20 19:02:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 if (fs.existsSync("config.env")) require("dotenv").config({ path: "./config.env" });
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your MongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "94779912589", // Princess Olya Owner Number
+  SESSION_ID: process.env.SESSION_ID || "O0ZkgaKb#crGGARGcpaaoWHmg0_G3XERVnHlKa_ifk4Dgc67CW3Q",
+  MONGODB: process.env.MONGODB || "mongodb+srv://Queen-Aurora:Pasanhansaka2007@cluster0.x2ktu.mongodb.net/",
+  OWNER_NUM: process.env.OWNER_NUM || "94720573770", // Princess Olya Owner Number
   BOT_NAME: "Princess Olya",
   PREFIX: ".", // Command Prefix
 };
